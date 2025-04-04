b45c34a2d7383db4dd8b5da5ac97cb4761fc42df
SergioCaMi <sergiocami84@gmail.com>
2025-04-04 11:05:59 +0200

    fin uf1842 DOM

--- uf1842/2-DOM/js/app.js
+++ uf1842/2-DOM/js/app.js
@@ -1,3 +1,10 @@
 /**
  * ¡Buena suerte! ¡Pasa el mensaje!
  */
+const inputMessage = document.getElementById("message");
+const ouputMessage = document.getElementById("last-message");
+document.getElementById("btn-send").addEventListener("click", e=>{
+    ouputMessage.textContent = inputMessage.value;
+    inputMessage.value = '';
+    inputMessage.focus();
+});
