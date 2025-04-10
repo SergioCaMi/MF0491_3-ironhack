bf0fd8ea5b46fe801d01853debfdac8aed3775ad
SergioCaMi <sergiocami84@gmail.com>
2025-04-10 17:57:17 +0200

    revision ok

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,7 +1,6 @@
 async function getPuzzle(wordCount) {
     const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
     const hiddenSentence = await response.json();
-    console.log("Palabra: " + hiddenSentence.puzzle);
     return hiddenSentence.puzzle;
 
 }
